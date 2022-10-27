692215aea6d438b71c198567202e12a89f2dd832
Niall Sauvage <sauvagen@tcd.ie>
2022-10-27 01:41:25 +0100

    Added getPrecedence, isRightAssociative, associated unit tests

--- src/main/java/com/spring/javawebserver/webserver/Arith.java
+++ src/main/java/com/spring/javawebserver/webserver/Arith.java
@@ -18,18 +18,43 @@ public class Arith {
     }
 
     public static boolean isOperand(String possibleOperand) {
-        return false;
+        if(possibleOperand.charAt(0) == '-'){
+            possibleOperand = possibleOperand.substring(1, possibleOperand.length());
+        }
+        for (int i = 0; i < possibleOperand.length(); i++){
+            if (possibleOperand.charAt(i) < '0' || possibleOperand.charAt(i) > '9'){
+                return false;
+            }
+        }
+        return possibleOperand.length() >= 1;
     }
 
     public static boolean isOperator(String possibleOperator) {
-        return false;    
+        String operators = "+-/*^";
+        return (operators.contains(possibleOperator) && possibleOperator.length() == 1);
     }
 
-    public static boolean isRightAssociate(String operator) {
+    public static boolean isRightAssociative(String operator) {
+        if(!isOperator(operator))
+            throw new IllegalArgumentException();
+        if (operator == "^"){
+            return true;
+        }
         return false;
     }
 
     public static int getPrecedence(String operator) {
-        return -1;
+        switch (operator){
+            case "^":
+                return 3;
+            case "*":
+            case "/":
+                return 2;
+            case "+":
+            case "-":
+                return 1;
+            default:
+                throw new IllegalArgumentException();
+        }
     }
 }
--- src/test/java/com/spring/javawebserver/webserver/CalculatorApplicationTests.java
+++ src/test/java/com/spring/javawebserver/webserver/CalculatorApplicationTests.java
@@ -26,18 +26,64 @@ class CalculatorApplicationTests {
 	}
 	@Test
 	public void testIsOperand(){
-
+		String operand = "1";
+		assertEquals(true, Arith.isOperand(operand), "Testing isOperand with valid single-digit operand with no minus");
+		operand = "-1";
+		assertEquals(true, Arith.isOperand(operand), "Testing isOperand with valid single-digit operand and minus");
+		operand = "100";
+		assertEquals(true, Arith.isOperand(operand), "Testing isOperand with valid multi-digit operand with no minus");
+		operand = "-100";
+		assertEquals(true, Arith.isOperand(operand), "Testing isOperand with valid multi-digit operand and minus");
+		operand = "-";
+		assertEquals(false, Arith.isOperand(operand), "Testing isOperand with invalid string");
+		operand = "1-0";
+		assertEquals(false, Arith.isOperand(operand), "Testing isOperand with invalid string");
+		operand = "10-";
+		assertEquals(false, Arith.isOperand(operand), "Testing isOperand with invalid string");
 	}
 	@Test 
 	public void testIsOperator(){
-
+		String operator = "+";
+		assertEquals(true, Arith.isOperator(operator), "Testing isOperator with valid operator");
+		operator = "-";
+		assertEquals(true, Arith.isOperator(operator), "Testing isOperator with valid operator");
+		operator = "*";
+		assertEquals(true, Arith.isOperator(operator), "Testing isOperator with valid operator");
+		operator = "/";
+		assertEquals(true, Arith.isOperator(operator), "Testing isOperator with valid operator");
+		operator = "^";
+		assertEquals(true, Arith.isOperator(operator), "Testing isOperator with valid operator");
+		operator = "-1";
+		assertEquals(false, Arith.isOperator(operator), "Testing isOperator with single-digit operand and minus");
+		operator = "100";
+		assertEquals(false, Arith.isOperator(operator), "Testing isOperator with multi-digit operand with no minus");
+		operator = "-100";
+		assertEquals(false, Arith.isOperator(operator), "Testing isOperator with multi-digit operand and minus");
 	}
 	@Test
 	public void testGetPrecedence(){
-
+		String operator = "+";
+		assertEquals(1, Arith.getPrecedence(operator), "Testing getPrecedence with addition");
+		operator = "-";
+		assertEquals(1, Arith.getPrecedence(operator), "Testing getPrecedence with subtraction");
+		operator = "*";
+		assertEquals(2, Arith.getPrecedence(operator), "Testing getPrecedence with multiplication");
+		operator = "/";
+		assertEquals(2, Arith.getPrecedence(operator), "Testing getPrecedence with division");
+		operator = "^";
+		assertEquals(3, Arith.getPrecedence(operator), "Testing getPrecedence with power");
 	}
 	@Test 
 	public void testIsRightAssociative(){
-		
+		String operator = "+";
+		assertEquals(false, Arith.isRightAssociative(operator), "Testing isRightAssociative with addition");
+		operator = "-";
+		assertEquals(false, Arith.isRightAssociative(operator), "Testing isRightAssociative with subtraction");
+		operator = "*";
+		assertEquals(false, Arith.isRightAssociative(operator), "Testing isRightAssociative with multiplication");
+		operator = "/";
+		assertEquals(false, Arith.isRightAssociative(operator), "Testing isRightAssociative with division");
+		operator = "^";
+		assertEquals(true, Arith.isRightAssociative(operator), "Testing isRightAssociative with power");
 	}
 }
