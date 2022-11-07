96e49adeaa431c7d8a35b454d57a8fbb5e03fa8d
Fenlo <55544169+JamesFenzy09@users.noreply.github.com>
2022-11-07 10:42:59 +0000

    variables fixed,comments added,to the power added

--- src/main/resources/static/javascript/script.js
+++ src/main/resources/static/javascript/script.js
@@ -1,50 +1,48 @@
+/*START OF THE BUTTON TOGGLE SWITCH BETWEEN THE STRING INPUT AND THE CALCULATOR DISPLAY*/
 function myFunction() {
   //Calculator input dissappears and so does the text area
-  var x = document.getElementById("stringInput");
-  var y = document.getElementById("calcButtons");
-  var z = document.getElementById("strButtons");
-  var k = document.getElementById("calcDisplay");
-  var l = document.getElementById("userExpression");
-  var m = document.getElementById("fixStyle");
-  x.style.display = "none";
-  z.style.backgroundColor = "lightGray";
-  y.style.backgroundColor = "lightblue";
-  k.style.display = "block";
-  l.style.display = "none";
-  m.style.width = "100%";
+  var stringInput = document.getElementById("stringInput");
+  var calcButtons = document.getElementById("calcButtons");
+  var strButtons = document.getElementById("strButtons");
+  var calcDisplay = document.getElementById("calcDisplay");
+  var userExpression = document.getElementById("userExpression");
+  var fixStyle = document.getElementById("fixStyle");
+  stringInput.style.display = "none";
+  strButtons.style.backgroundColor = "lightGray";
+  calcButtons.style.backgroundColor = "lightblue";
+  calcDisplay.style.display = "block";
+  userExpression.style.display = "none"; //THIS IS ONLINE IN TO HIDE THE TEXT AREA THE BUTTON IS GETTING THE EXPRESSION FROM--IT IS BEING DISPLAYED BETTER AS THE TOP OF THE CALCULATOR
+  fixStyle.style.width = "100%";
 }
 function strFunction() {
   //String input dissappears and so does the calculator design
-  var x = document.getElementById("stringInput");
-  var y = document.getElementById("strButtons");
-  var z = document.getElementById("calcButtons");
-  var k = document.getElementById("calcDisplay");
-  x.style.display = "block";
-  z.style.backgroundColor = "lightGray";
-  y.style.backgroundColor = "lightblue";
-  k.style.display = "none";
+  var stringInput = document.getElementById("stringInput");
+  var strButtons = document.getElementById("strButtons");
+  var calcButtons = document.getElementById("calcButtons");
+  var calcDisplay = document.getElementById("calcDisplay");
+  stringInput.style.display = "block";
+  calcButtons.style.backgroundColor = "lightGray";
+  strButtons.style.backgroundColor = "lightblue";
+  calcDisplay.style.display = "none";
 }
+/*END OF THE BUTTON TOGGLE SWITCH BETWEEN THE STRING INPUT AND THE CALCULATOR DISPLAY -*/
 
-// Initializes a new instance of the StringBuilder class
-// and appends the given value if supplied
+/*START OF THE STRING BUILDER CLASS IN ORDER TO KEEP ADDING LETTER BY LETTER UNITL THE USER HITS THE '=' BUTTOIN*/
 function StringBuilder(value) {
   this.strings = new Array("");
   this.append(value);
 }
 
-// Appends the given value to the end of this instance.
 StringBuilder.prototype.append = function (value) {
   if (value) {
     this.strings.push(value);
   }
 };
 
-// Clears the string buffer
 StringBuilder.prototype.clear = function () {
   this.strings.length = 1;
 };
 
-// Converts this instance to a String.
 StringBuilder.prototype.toString = function () {
   return this.strings.join("");
 };
@@ -60,3 +58,4 @@ function clearCalc() {
   document.getElementById("userExpression").innerHTML = sb.toString();
   document.getElementById("userDisplay").innerHTML = sb.toString();
 }
+/*END OF THE STRING BUILDER CLASS IN ORDER TO KEEP ADDING LETTER BY LETTER UNITL THE USER HITS THE '=' BUTTOIN*/
